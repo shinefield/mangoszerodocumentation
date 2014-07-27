4d6fe6deb4f48dab61ad5640f94c166f7b550751
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-07-27 20:31:25 +0200

    Updated credits for Sphinx theme.
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'mangos-zero'
-copyright = u'2013-2014, Daniel S. Reichenbach'
+copyright = u'2014, Daniel S. Reichenbach'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
